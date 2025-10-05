94b9aa4e82b81a3862797a5aa64daed98bfcb2b6
Teika7778 <max2005swet@gmail.com>
2025-10-06 01:46:44 +0300

    main fix

--- main.cpp
+++ main.cpp
@@ -3,12 +3,24 @@
 #include <cmath>
 
 int main() {
-    kepler_orbit_denorm d;
+    kepler_orbit_denorm denorm;
+
+    denorm.a = 0.121;
+    denorm.e = 0.872;
+    denorm.w = 68.9;
+    denorm.omega = 231.9;
+    denorm.i = 138.1;
+    denorm.T0 = 2002.27;
+    denorm.t0 = 2002.9;
+
+    double R_0 = 9460730472580800.0 * 26996; // distance in meters
+    double M_0 = 8.54e36; // mass in kg
+
     kepler_orbit n;
 
     double grav = 0;
 
-    //normalize(&d, &n);
+    normalize(&denorm, &n, R_0, M_0);
     int t0 = 5;
     int step = 5;
     int steps = 6;
