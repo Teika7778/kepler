01b478a7b531b66013622981848b32987d8eea33
Teika7778 <max2005swet@gmail.com>
2025-10-06 01:52:44 +0300

    main cycle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "transform.h"
 #include "normalize.h"
+#define G 6.67428e-11
 #include <cmath>
 
 int main() {
@@ -18,11 +19,8 @@ int main() {
 
     kepler_orbit n;
 
-    double grav = 0;
-
-    normalize(&denorm, &n, R_0, M_0);
     int t0 = 5;
-    int step = 5;
+    int step = 5000;
     int steps = 6;
     int m_val = t0 + step*steps;
 
@@ -31,33 +29,14 @@ int main() {
     double ra, dec;
 
     for (int t = t0; t < m_val; t += step) {
+        denorm.t0 = 2002.9 + (double) t;
+        normalize(&denorm, &n, R_0, M_0);
+        double grav = sqrt(M_0*G/pow(n.a, 3));
         kepler_to_cart(&n, t, grav, pos, velo);
+
+
         ra = atan2(pos[2], sqrt(pow(pos[0],2) + pow(pos[1],2)));
         dec = atan2(pos[1], pos[0]);
         std::cout << ra << " " << dec << std::endl;
     }
 }
-/*
-int main() {
-
-    kepler_orbit_denorm* denorm;
-
-    denorm->a = 0.121;
-    denorm->e = 0.872;
-    denorm->w = 68.9;
-    denorm->omega = 231.9;
-    denorm->i = 138.1;
-    denorm->T0 = 2002.27;
-    denorm->t0 = 2002.9;
-
-    double R_0 = 9460730472580800.0 * 26996; // distance in meters
-    double M_0 = 8.54e36; // mass in kg
-
-    kepler_orbit* norm;
-
-    normalize(denorm, norm, R_0, M_0);
-
-    return 0;
->>>>>>> 6c2534dbc40686d556eccff6cebf9d468cb63592
-}
-*/
