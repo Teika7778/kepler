a9915cd289bac2b551a357a19c5a6f755945dbe7
Teika7778 <max2005swet@gmail.com>
2025-10-10 17:31:14 +0300

    cord shift

--- constans.hpp
+++ constans.hpp
@@ -2,8 +2,10 @@
 
 #define G 6.67428e-11 // gravitational constant
 
-#define M_BH 8.54e36  // Mass of Sgr A* in kg
-
-#define R_BH_LY 26996 // Distance to Sgr A* in light years
+#define M_BH     8.54e36      // Mass of Sgr A* in kg
+#define R_BH_LY  26996        // Distance to Sgr A* in light years
+#define DEC_BH  -0.506282     // DEC of Sgr A*
+#define RA_BH    4.649850     // RA  of Sgr A*
 
 #define LIGHT_YEAR 9460730472580800 // Meters in light year
+
--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main() {
         231.9,  // omega
         138.1,  // i
         2002.27, // T0
-        2002.27  //t0 
+        2002.27  //t0
     };
 
     int fractioning = 1000;
@@ -36,34 +36,38 @@ int main() {
     double pos[3];
     double cur_ra, cur_dec, prev_ra, prev_dec;
 
+    double d = R_BH_LY * LIGHT_YEAR;
+    double BH_x = d * cos(DEC_BH) * cos(RA_BH);
+    double BH_y = d * cos(DEC_BH) * sin(RA_BH);
+    double BH_z = d * sin(DEC_BH);
+
     FILE* file = fopen("angles.txt", "w");
 
     if (!file) return 0;
 
     for (size_t i = 0; i< fractioning; i++)
     {
-        
+
         denorm_orbit.t0 = denorm_orbit.T0 + i*delta_t;
         normalize(&denorm_orbit, &orbit, R_BH_LY, M_BH);
 
         double grav = sqrt( (M_BH/pow(orbit.a, 3)) * G );
         kepler_to_cart(&orbit, grav, pos, velo);
+        pos[0] += BH_x;
+        pos[1] += BH_y;
+        pos[2] += BH_z;
 
         cur_ra = atan2(pos[2], sqrt(pow(pos[0],2) + pow(pos[1],2))) * 180.0 / M_PI;
         cur_dec = atan2(pos[1], pos[0]) * 180.0 / M_PI;
 
-        
 
-        while (cur_ra - prev_ra > 180) cur_ra -= 360;
-        while (cur_ra - prev_ra < -180) cur_ra += 360;
-        
-        // Корректировка склонения  
-        while (cur_dec - prev_dec > 180) cur_dec -= 360;
-        while (cur_dec - prev_dec < -180) cur_dec += 360;
+
+
 
         if ( i != 0)
         {
-            fprintf(file, "%f %f\n", cur_ra-prev_ra, cur_dec-prev_dec);
+            // fprintf(file, "%f %f\n", cur_ra, cur_dec);
+            fprintf(file, "%f %f\n", cur_ra - RA_BH * 180.0 / M_PI, cur_dec - DEC_BH * 180.0 / M_PI);
         }
 
         prev_ra = cur_ra;
